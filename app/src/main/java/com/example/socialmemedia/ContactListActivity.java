2910649a7d1f6caa421108359cb476a6757b1d1a
YutingShang <18shang.yt@gmail.com>
2021-09-28 20:26:47 +0100

    ArrayAdapter and ContactListView Social Memedia V 1.0

--- app/src/main/java/com/example/socialmemedia/ContactListActivity.java
+++ app/src/main/java/com/example/socialmemedia/ContactListActivity.java
@@ -3,11 +3,17 @@ package com.example.socialmemedia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.ArrayAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ContactListActivity extends AppCompatActivity {
 
     ListView contactListView;
+    ArrayAdapter contactsAdapter;
+    ArrayList<String> users = new ArrayList<String>(Arrays.asList("Ori","Joe","Ben","Bob","Ned","Tim","Uma","Mia","Edi","Zak","Ali","Tom","Max","Pip","Dan","Kev","Jil","Ido"));
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -15,6 +21,10 @@ public class ContactListActivity extends AppCompatActivity {
         setContentView(R.layout.activity_contact_list);
 
         contactListView=findViewById(R.id.contactListView);
+        contactsAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,users);
+        //ArrayAdapter populates a ListView with ArrayList items
+        contactListView.setAdapter(contactsAdapter);
+
 
     }
 }

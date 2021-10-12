11e08ddb664f06e6fe7a4f38992d7c1ae2b6d799
YutingShang <18shang.yt@gmail.com>
2021-10-12 23:00:02 +0100

    Activity navigation bar with colours Social Memedia V 1.0

--- app/src/main/java/com/example/socialmemedia/ContactListActivity.java
+++ app/src/main/java/com/example/socialmemedia/ContactListActivity.java
@@ -1,14 +1,18 @@
 package com.example.socialmemedia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -19,6 +23,8 @@ public class ContactListActivity extends AppCompatActivity {
     ArrayAdapter contactsAdapter;
     ArrayList<String> users = new ArrayList<String>(Arrays.asList("Ori","Joe","Ben","Bob","Ned","Tim","Uma","Mia","Edi","Zak","Ali","Tom","Max","Pip","Dan","Kev","Jil","Ido"));
     ImageButton settingsButton;
+    BottomNavigationView bottomNavigationView;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -48,6 +54,23 @@ public class ContactListActivity extends AppCompatActivity {
             public void onClick(View v) {
                 Intent intent2 = new Intent(ContactListActivity.this, SettingsActivity.class);
                 startActivity(intent2);
+            }
+        });
+
+        bottomNavigationView= findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.contactList);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.memeMenu:
+                        startActivity(new Intent(getApplicationContext(),MemeMenuActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.contactList:
+                        return true;
+                }
+                return false;
             }
         });
     }
--- app/src/main/java/com/example/socialmemedia/MemeMenuActivity.java
+++ app/src/main/java/com/example/socialmemedia/MemeMenuActivity.java
@@ -1,14 +1,39 @@
 package com.example.socialmemedia;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MemeMenuActivity extends AppCompatActivity {
+
+
+    BottomNavigationView bottomNavigationView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_meme_menu);
+
+        bottomNavigationView= findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setSelectedItemId(R.id.memeMenu);
+        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
+            @Override
+            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+                switch(item.getItemId()){
+                    case R.id.contactList:
+                        startActivity(new Intent(getApplicationContext(),ContactListActivity.class));
+                        overridePendingTransition(0,0);
+                        return true;
+                    case R.id.memeMenu:
+                        return true;
+                }
+                return false;
+            }
+        });
     }
 }

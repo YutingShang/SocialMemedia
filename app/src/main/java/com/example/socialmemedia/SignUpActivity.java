76bdff44e7a8bd05bc1456e7327d8945a75bb8fb
YutingShang <18shang.yt@gmail.com>
2021-11-06 23:14:43 +0000

    AppBar and SignUp/SignIn Tabs Social Memedia  V 2.0

--- app/src/main/java/com/example/socialmemedia/SignUpActivity.java
+++ app/src/main/java/com/example/socialmemedia/SignUpActivity.java
@@ -1,33 +1,52 @@
 package com.example.socialmemedia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.FragmentPagerAdapter;
+import androidx.viewpager.widget.ViewPager;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
+import com.google.android.material.tabs.TabLayout;
+
 public class SignUpActivity extends AppCompatActivity {
 
     Button signUp;
+    private TabLayout tabLayout;
+    private ViewPager viewPager;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
 
-        signUp = findViewById(R.id.signUpButton);
+        tabLayout=findViewById(R.id.tab_layout);
+        viewPager = findViewById(R.id.viewPager);
 
-        signUp.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                openContactList();
-            }
-        });
+        tabLayout.setupWithViewPager(viewPager);
+
+        ViewPagerAdapter viewPagerAdapter = new ViewPagerAdapter(getSupportFragmentManager(), FragmentPagerAdapter.BEHAVIOR_RESUME_ONLY_CURRENT_FRAGMENT);
+        viewPagerAdapter.addFragment(new signInFragment(),"Sign In");
+        viewPagerAdapter.addFragment(new signUpFragment(),"Sign Up");
+
+        viewPager.setAdapter(viewPagerAdapter);
+
+
+//        signUp = findViewById(R.id.signUpButton);
+//
+//        signUp.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                openContactList();
+//            }
+//        });
     }
 
-    public void openContactList(){
-        Intent intent = new Intent(SignUpActivity.this, ContactListActivity.class);
-        startActivity(intent);
-    }
+//    public void openContactList(){
+//        Intent intent = new Intent(SignUpActivity.this, ContactListActivity.class);
+//        startActivity(intent);
+//    }
 }
---
+++ app/src/main/java/com/example/socialmemedia/signUpFragment.java
@@ -0,0 +1,43 @@
+package com.example.socialmemedia;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.Button;
+
+
+public class signUpFragment extends Fragment {
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_sign_up, container, false);
+
+    }
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+    public void onViewCreated(View view, Bundle savedInstanceState){
+        super.onViewCreated(view,savedInstanceState);
+
+        Button signUp= getView().findViewById(R.id.signUpButton);
+
+        signUp.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(v.getContext(), ContactListActivity.class);
+                startActivity(intent);
+            }
+        });
+    }
+}
